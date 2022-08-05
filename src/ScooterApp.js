0214252e96896d9122fc0fc5587cf03f86e1316c
Elise <108088941+Elxsemn@users.noreply.github.com>
2022-08-05 17:50:34 +0100

    first commit

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,29 @@ const Scooter = require('./Scooter');
 
 class ScooterApp {
   // ScooterApp code here
+
+    constructor () {
+        this.users = []
+    }
+
+    signUp (username, password, age) {
+//Does the user exist 
+if (this.users.find(u => u.username === username)) throw new Error('User already exists')
+// Are they old enough?
+if (age < 18) throw new Error('Must be 18 or over.')
+const user = new user(username, password, age)
+this.users.push(user)
+return user
+    }
+logIn (username, password) {
+    const user = this.users.find(u => u.username === username)
+    if (!user) throw new Error('Please sign up first')
+    if (user.password !== password) throw new Error ('Cannot sign in.')
+    user.loggedIn = true
+}
+
+
+
 }
 
 module.exports = ScooterApp;
--- src/User.js
+++ src/User.js
@@ -1,5 +1,26 @@
 class User {
     // User code here
+      #password
+      constructor(username, password, email) {
+          this.username = username
+          this.age = this.age
+          this.#password = password
+          this.email = email
+      }
+  
+  
+      rentScooter (scooterID) {
+          book.isAvailable = false
+          book.currentOwner = this.username
+          this.scooterID.push(scooters)
+      }
+  
+      returnScooter (scooterID) {
+          scooterID.isAvailable = true
+          scooterID.currentOwner = null
+      }
+  
   }
+
   
   module.exports = User
